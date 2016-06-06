662ee93448f1b378470dd6e41067ef32edbb0a4c
TheMonkeyz <rockmonkeyz@gmail.com>
2016-06-05 22:44:30 -0400

    Allow specifying an alternate scp user

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,11 @@ echo $AUTHORIZED_KEYS | base64 -d >>$AUTHORIZED_KEYS_FILE
 # Chown data folder (if mounted as a volume for the first time)
 chown data:data /home/data
 
+if [ -z $SCP_USER ];
+then
+  chown $SCP_USER /authorized_keys
+  usermod --shell /usr/bin/rssh 
+fi
+
 # Run sshd on container start
 exec /usr/sbin/sshd -D -e
